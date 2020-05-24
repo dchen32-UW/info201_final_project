b2445685234b6935fc250e55d00bc74fc0f52718
dchen32-UW <dchen32@uw.edu>
2020-05-24 11:02:56 -0700

    add latest changes to index

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,10 +1,4 @@
 library(dplyr)
-source("data_cleaning.R")
-
-# DATASET 1
-# get clean average temperature by country dataset
-avg_country_temp_data <-
-  get_clean_avg_country_temp(avg_temp_by_country)
 
 get_summary_info_temp <- function(dataset) {
   result <- list()
@@ -12,19 +6,8 @@ get_summary_info_temp <- function(dataset) {
   return(result)
 }
 
-temp_summary_info <- get_summary_info_temp(avg_country_temp_data)
-
-# DATASET 2
-# get clean combined natural disaster count, damage, and temp dataset
-natural_disaster_data <-
-  get_clean_natural_disaster(global_temp,
-                             natural_disaster_count,
-                             natural_disaster_damage)
-
 get_summary_info_disaster <- function(dataset) {
   result <- list()
   result$length <- nrow(dataset)
   return(result)
 }
-
-disaster_summary_info <- get_summary_info_disaster(natural_disaster_data)
