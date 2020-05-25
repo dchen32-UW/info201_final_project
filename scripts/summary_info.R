2888f240a7e7fd2e60563b771404ebe18e67928e
Grace Teebken <gteebken@uw.edu>
2020-05-25 15:11:06 -0700

    Finish summary function for dataset 2 and add summary description in index.Rmd

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,13 +1,51 @@
 library(dplyr)
 
+#FUNCTION NOT FINISHED
 get_summary_info_temp <- function(dataset) {
   result <- list()
   result$length <- nrow(dataset)
+  #range of years
+  result$range <- dataset %>%
+    summarize(max_year = max(year, na.rm = TRUE),
+              min_year = min(year, na.rm = TRUE))
+  #number of unique countries (group_by and then n())
+  result$regions <- dataset %>%
+    group_by(region) %>%
+    summarize(regions = n_distinct(region)) %>%
+    nrow()
+  #country highest temp after 1988 (filter for >1988)
+  result$country_highest_after_1988 <- dataset %>%
+    filter(year > 1988, avg_temp == max(avg_temp, na.rm = TRUE))
+  #country highest temp before 1988 (filter for <= 1988)
+  #country highest temp for all years
   return(result)
 }
 
+#FUNCTION IS FINISHED
 get_summary_info_disaster <- function(dataset) {
   result <- list()
   result$length <- nrow(dataset)
+  #range of years
+  result$range <- dataset %>%
+    summarize(max_year = max(year, na.rm = TRUE),
+              min_year = min(year, na.rm = TRUE))
+  #year with most damage
+  result$year_highest_economic_damage <- dataset %>%
+    filter(damage == max(damage, na.rm = TRUE)) %>%
+    slice(1)
+  #year with most disasters
+  result$year_highest_count <- dataset %>%
+    filter(count == max(count, na.rm = TRUE)) %>%
+    slice(1)
+  #year with highest temp
+  result$year_highest_temp <- dataset %>%
+    filter(mean_land_ocean_temp == max(mean_land_ocean_temp, na.rm = TRUE)) %>%
+    slice(1)
+  #year with most damage per disaster
+  result$year_highest_impact <- dataset %>%
+    filter(disaster == "All natural disasters") %>%
+    mutate(impact = damage / count) %>%
+    filter(impact == max(impact, na.rm = TRUE)) %>%
+    slice(1)
   return(result)
 }
