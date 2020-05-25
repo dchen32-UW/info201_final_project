0d785659c92ab16a5ff4d14b3996a0a6cfa83b04
dchen32-UW <dchen32@uw.edu>
2020-05-25 15:42:08 -0700

    change facet wrap to make plot 3 look better

--- scripts/plot3.R
+++ scripts/plot3.R
@@ -23,7 +23,8 @@ scatter_nd_count_by_temp <- function(nd_data) {
                              y = count,
                              color = damage)) +
     facet_wrap(~disaster,
-               scales="free_y") +
+               scales="free_y",
+               nrow = 4) +
     scale_color_gradientn(colours = rainbow(5)) +
     labs(color = "Normalized Damage",
          title = paste0("Disaster Count by Mean Land Temperature ",
