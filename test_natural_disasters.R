2f068a46b4cfcf21134a891f8a3648f240580b54
dchen32-UW <dchen32@uw.edu>
2020-05-24 20:52:39 -0700

    remove origin plot3 from temp file

--- test_natural_disasters.R
+++ test_natural_disasters.R
@@ -19,23 +19,3 @@ trendline_nd_count_by_year <- function(nd_data) {
   return(plot)
 }
 
-# PLOT 3 - plots a split by disaster type scatter plot of each
-#   natural disaster, excluding all natural disasters, showing
-#   count of natural disaster by average land temperature with
-#   the damage the natural disasters for that year caused colored on
-# parameters:
-#   - nd_data = dataframe of the natural disaster data, combined with
-#               the global land/land+ocean temperature per year dataframe
-scatter_nd_count_by_temp <- function(nd_data) {
-  plot <-
-    nd_data %>%
-    filter(disaster != "All natural disasters") %>%
-    ggplot() +
-      geom_point(mapping = aes(x = mean_land_temp,
-                               y = count,
-                               color = damage)) +
-      facet_wrap(~disaster) +
-      scale_color_gradientn(colours = rainbow(5))
-
-  return(plot)
-}
