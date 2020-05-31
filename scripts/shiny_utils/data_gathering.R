32b450bb2731c86973e5835f206b17ea0d8e47eb
dchen32-UW <dchen32@uw.edu>
2020-05-31 13:42:15 -0700

    add wasserstein world maps

--- scripts/shiny_utils/data_gathering.R
+++ scripts/shiny_utils/data_gathering.R
@@ -5,6 +5,8 @@ source("scripts/shiny_utils/data_cleaning.R")
 
 # gather the average country temperature data, on a monthly scale
 #   using shiny utils' data cleaning functions
+# returns:
+#   - temp_data : dataframe of the temperature measurments per country
 get_avg_country_temp_data <- function() {
   # DATASET 1
   # gather average land temperature by country
@@ -18,4 +20,27 @@ get_avg_country_temp_data <- function() {
     get_clean_avg_country_temp(avg_temp_by_country)
 
   return(avg_country_temp_data)
+}
+
+# assign mega region to temp data and removes the rows that are NA
+#   also known as countries that did not have a mega region
+# parameters:
+#   - temp_data : dataframe of the temperature measurments per country
+# returns:
+#   - temp_data : filtered input with mega regions column
+get_mega_region_temp_data <- function(temp_data) {
+  # assign mega regions
+  temp_data <-
+    temp_data %>%
+    mutate(
+      mega_region =
+        temp_data %>%
+        pull(region) %>%
+        countrycode("country.name",
+                    "region")
+    )
+  # remove NA values, basically countries that did not match to continents
+  temp_data <- temp_data[complete.cases(temp_data), ]
+
+  return(temp_data)
 }
