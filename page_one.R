2eb55f72bdb5d5a670625e3781ed3df9865c82ef
dchen32-UW <dchen32@uw.edu>
2020-06-01 15:17:52 -0700

    add paragraphs 1 and 2 for page 1

--- page_one.R
+++ page_one.R
@@ -5,10 +5,6 @@ library(plotly)
 # import constants
 source("scripts/shiny_utils/constants.R")
 # import text to write
-main_panel_para_1 <-
-  readChar("texts/p1_para1.txt", file.info("texts/p1_para1.txt")$size)
-main_panel_para_2 <-
-  readChar("texts/p1_para2.txt", file.info("texts/p1_para2.txt")$size)
 main_panel_para_3 <-
   readChar("texts/p1_para3.txt", file.info("texts/p1_para3.txt")$size)
 main_panel_para_4 <-
@@ -16,7 +12,13 @@ main_panel_para_4 <-
 main_panel_para_5 <-
   readChar("texts/p1_para5.txt", file.info("texts/p1_para5.txt")$size)
 
-# get wasserstein images
+# get HTML to write
+main_panel_para_1 <-
+  readChar("html/p1_para1.html",
+           file.info("html/p1_para1.html")$size)
+main_panel_para_2 <-
+  readChar("html/p1_para2.html",
+           file.info("html/p1_para2.html")$size)
 wasserstein_earths <-
   readChar("html/wasserstein_earths.html",
            file.info("html/wasserstein_earths.html")$size)
@@ -34,11 +36,11 @@ page_one_heatmaps <- tabPanel(
   ),
   mainPanel(
     h3("Correlations Between Countries for All Recorded Temperatures"),
-    p(main_panel_para_1),
+    HTML(main_panel_para_1),
     plotlyOutput(outputId = "world_map_temp_mega_regions"),
-    p(main_panel_para_2),
+    HTML(main_panel_para_2),
     plotOutput(outputId = "all_ct_temp_mega_corrmap"),
-    p(main_panel_para_3),
+    HTML(main_panel_para_3),
     plotOutput(outputId = "all_ct_temp_grouped_corrmap"),
     plotlyOutput(outputId = "world_map_temp_grouped"),
     p(main_panel_para_4),
