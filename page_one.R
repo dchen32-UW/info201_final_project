d71fb56ad32ac9d63935c9ab386595966c7dd4b4
dchen32-UW <dchen32@uw.edu>
2020-06-01 16:43:15 -0700

    add deployment of shiny app to shinapps.io and add content to non emd corr on page 1

--- page_one.R
+++ page_one.R
@@ -4,13 +4,6 @@ library(plotly)
 
 # import constants
 source("scripts/shiny_utils/constants.R")
-# import text to write
-main_panel_para_3 <-
-  readChar("texts/p1_para3.txt", file.info("texts/p1_para3.txt")$size)
-main_panel_para_4 <-
-  readChar("texts/p1_para4.txt", file.info("texts/p1_para4.txt")$size)
-main_panel_para_5 <-
-  readChar("texts/p1_para5.txt", file.info("texts/p1_para5.txt")$size)
 
 # get HTML to write
 main_panel_para_1 <-
@@ -19,6 +12,15 @@ main_panel_para_1 <-
 main_panel_para_2 <-
   readChar("html/p1_para2.html",
            file.info("html/p1_para2.html")$size)
+main_panel_para_3 <-
+  readChar("html/p1_para3.html",
+           file.info("html/p1_para3.html")$size)
+main_panel_para_4 <-
+  readChar("html/p1_para4.html",
+           file.info("html/p1_para4.html")$size)
+main_panel_para_5 <-
+  readChar("html/p1_para5.html",
+           file.info("html/p1_para5.html")$size)
 wasserstein_earths <-
   readChar("html/wasserstein_earths.html",
            file.info("html/wasserstein_earths.html")$size)
@@ -42,10 +44,10 @@ page_one_heatmaps <- tabPanel(
     plotOutput(outputId = "all_ct_temp_mega_corrmap"),
     HTML(main_panel_para_3),
     plotOutput(outputId = "all_ct_temp_grouped_corrmap"),
+    HTML(main_panel_para_4),
     plotlyOutput(outputId = "world_map_temp_grouped"),
-    p(main_panel_para_4),
+    HTML(main_panel_para_5),
     hr(),
-    p(main_panel_para_5),
     HTML(wasserstein_earths),
     plotlyOutput(outputId = "world_map_emd_mega_regions"),
     plotOutput(outputId = "all_ct_emd_mega_corrmap"),
