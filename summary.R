08b418ec83f2160c942c428b5569022c999f164e
“Tanner <tml13@uw.edu>
2020-06-02 18:58:17 -0700

    summary

--- summary.R
+++ summary.R
@@ -1,4 +1,31 @@
 library(shiny)
 
-#summary 1:
-#
+# Summary:
+
+
+# Take Away One:
+
+# Through out this assignment we learned how the eath has changed over the past
+# few years. This change is in temperatures, water levels, and more. We used 
+# different graphs to show how the climate has changed in different ways.
+
+
+# Take Away Two:
+
+# One of our graph talks about natural disasters. It shows how the amount of 
+# natural disasters have spiked over the years. What is cool about our graph
+# is you an choose the year or type of natural disater so you can focus on
+# certain ones or certain years. This is important to seeing the difference
+# between years between natural disaters.
+
+
+# Take Away Three:
+
+# The last graph shows the change in temperature. What is cool about this is we
+# added colors to it to show the difference in different countries and 
+# years. This helps the viewer look more into it and better understand how
+# the temperature has changed throughout the years.
+# what is cool is you can pick a specific country and look at that one
+# individually. Some countries have data farther back so it is interesting
+# to go to different years and still see the changes throughout the years.
+# Most data comes within the last 100 years so it is all fairly recent.
